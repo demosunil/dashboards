119ab97d537142f1a826a707a48e08bedabdb8b3
SUNILKUMAR PATEL <scpatel1908@gmail.com>
2023-03-01 22:04:48 +0000

    dashboard final commit

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -1,11 +1,35 @@
 class CurrenciesController < ApplicationController
+
 def first_currency
   @raw_data = open("https://api.exchangerate.host/symbols").read
   @parsed_data = JSON.parse(@raw_data)
   @symbols_hash =@parsed_data.fetch("symbols")
 
   @array_of_symbols = @symbols_hash.keys
-  render({:template => "currency_templates/step.one.html.erb"})
+  render({:template => "currency_templates/step_one.html.erb"})
 end
+def second_currency
+  @raw_data = open("https://api.exchangerate.host/symbols").read
+  @parsed_data = JSON.parse(@raw_data)
+  @symbols_hash =@parsed_data.fetch("symbols")
+#parameters: {:from_currency =>"ARS"}
+  @array_of_symbols = @symbols_hash.keys
+  @from_symbol = params.fetch("from_currency")
+  render({:template => "currency_templates/step_two.html.erb"})
+end
+def homepage
+  render({:template => "currency_templates/forex.html.erb"})
+end
+def convert_currency
+  @from_symbol =params.fetch("from_currency")
+  @to_symbol = params.fetch("to_currency")
+  @rate_data = open("https://api.exchangerate.host/convert?from
+  =#{@from_currency}&to=#{@to_symbol}").read
+  @parsed_data = JSON.parse(@rate_data)
+  @result= @parsed_data.fetch("info").fetch("rate")
+  
+  render({:template => "currency_templates/step_three.html.erb"})
 
+  
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get("/",{:controller => "currencies",:action => "homepage"})
   get("/forex",{:controller => "currencies",:action => "first_currency"})
+  get("/forex/:from_currency",{:controller => "currencies",:action => "second_currency"})
+  get("/forex/:from_currency/:to_currency",{:controller => "currencies",:action => "convert_currency"})
 end
